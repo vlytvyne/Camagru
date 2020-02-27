fda5f20f67e7eb320160ccad2671373a6f7d3012
Vadym <v.lytvynets61@gmail.com>
2020-02-27 14:06:03 +0200

    Gallery markup

---
+++ views/GalleryView.php
@@ -0,0 +1,45 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="/views/css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="/views/css/gallery.css"/>
+	<title>Gallery</title>
+</head>
+
+<body>
+
+	<?php include 'parts/NavBar.php' ?>
+
+	<div class="container">
+		<div class="row justify-content-around">
+			<div class="col-lg-3 col-md-5 card my_col ">
+				<img src="/photos/photo_1.jpeg">
+			</div>
+			<div class="col-lg-3 col-md-5 card my_col ">
+				<img src="/photos/photo_2.jpeg">
+			</div>
+
+			<div class="col-lg-3 col-md-5 card my_col ">
+				<img src="/photos/photo_3.png">
+			</div>
+
+			<div class="col-lg-3 col-md-5 card my_col ">
+				<img src="/photos/photo_4.jpeg">
+			</div>
+			<div class="col-lg-3 col-md-5 card my_col ">
+				<img src="/photos/photo_5.png">
+			</div>
+
+			<div class="col-lg-3 col-md-5 card my_col ">
+				<img src="/photos/photo_6.jpeg">
+			</div>
+
+		</div>
+	</div>
+
+</body>
+</html>
--- views/parts/NavBar.php
+++ views/parts/NavBar.php
@@ -1,27 +1,19 @@
+<nav class="navbar">
+	<a href="#" class="navbar-brand">Camagru</a>
+	<ul class="nav navbar-nav flex-row">
+
 <?php if (isset($_SESSION['user'])) : ?>
-	<nav class="navbar">
-		<a href="#" class="navbar-brand">Camagru</a>
-
-		<ul class="nav navbar-nav flex-row">
-			<li class="nav-item" style="margin-right: 16px">
-				<a href="/logIn/logUserOut" class="btn btn-outline-light">Log Out</a>
-			</li>
-		</ul>
-
-	</nav>
+		<li class="nav-item" style="margin-right: 16px">
+			<a href="/logIn/logUserOut" class="btn btn-outline-light">Log Out</a>
+		</li>
 <?php else: ?>
-	<nav class="navbar">
-		<a href="#" class="navbar-brand">Camagru</a>
+		<li class="nav-item" style="margin-right: 16px">
+			<a href="/logIn" class="btn btn-outline-light">Log In</a>
+		</li>
 
-		<ul class="nav navbar-nav flex-row">
-			<li class="nav-item" style="margin-right: 16px">
-				<a href="/logIn" class="btn btn-outline-light">Log In</a>
-			</li>
-
-			<li class="nav-item">
-				<a href="/signUp" class="btn btn-primary">Sign Up</a>
-			</li>
-		</ul>
-
-	</nav>
+		<li class="nav-item">
+			<a href="/signUp" class="btn btn-primary">Sign Up</a>
+		</li>
 <?php endif; ?>
+	</ul>
+</nav>
